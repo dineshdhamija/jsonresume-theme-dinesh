fbd0cc001fff047619b7c52bf0a318d6e84ade37
Dinesh Dhamija <dineshd@noom.com>
2023-03-08 23:46:34 -0800

    More changes to layout and styles

--- index.js
+++ index.js
@@ -50,10 +50,8 @@ function render(resume) {
     }
   });
 
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
 	return Handlebars.compile(tpl)({
-		css: css,
 		resume: resume
 	});
 }
